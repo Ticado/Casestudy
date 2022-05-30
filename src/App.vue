6ffb42a92a8ee57bbc30ddda4b24d7468019fc4d
Ricardo <73585810+Ticado@users.noreply.github.com>
2022-05-30 16:01:31 +0200

    ProgressBar component aangemaakt. random colors voor ProgressBar.

--- src/App.vue
+++ src/App.vue
@@ -22,10 +22,170 @@
             </FeaturedCard>
           </v-col>
           <v-col :cols="12" md="auto grow">
-            <v-card> content </v-card>
+            <v-card> Behaalde badges </v-card>
+
+            <v-row>
+              <v-col>
+                <FeaturedCard>
+                  <FeaturedCardTop>
+                    <div>Persoonlijke punten</div>
+                    <FeaturedCardStat>32</FeaturedCardStat>
+                  </FeaturedCardTop>
+                  <ProgressBar> </ProgressBar>
+                  <v-card-text>
+                    Behaalde punten
+                    <Badge />
+                  </v-card-text>
+                </FeaturedCard>
+              </v-col>
+
+              <v-col>
+                <FeaturedCard>
+                  <FeaturedCardTop>
+                    <div>Persoonlijke punten</div>
+                    <FeaturedCardStat>32</FeaturedCardStat>
+                  </FeaturedCardTop>
+                  <ProgressBar> </ProgressBar>
+                  <v-card-text>
+                    Behaalde punten
+                    <Badge />
+                  </v-card-text>
+                </FeaturedCard>
+              </v-col>
+
+              <v-col>
+                <FeaturedCard>
+                  <FeaturedCardTop>
+                    <div>Persoonlijke punten</div>
+                    <FeaturedCardStat>32</FeaturedCardStat>
+                  </FeaturedCardTop>
+                  <ProgressBar> </ProgressBar>
+                  <v-card-text>
+                    Behaalde punten
+                    <Badge />
+                  </v-card-text>
+                </FeaturedCard>
+              </v-col>
+            </v-row>
+
+            <v-row>
+              <v-col>
+                <FeaturedCard>
+                  <FeaturedCardTop>
+                    <div>Persoonlijke punten</div>
+                    <FeaturedCardStat>32</FeaturedCardStat>
+                  </FeaturedCardTop>
+                  <ProgressBar> </ProgressBar>
+                  <v-card-text>
+                    Behaalde punten
+                    <Badge />
+                  </v-card-text>
+                </FeaturedCard>
+              </v-col>
+
+              <v-col>
+                <FeaturedCard>
+                  <FeaturedCardTop>
+                    <div>Persoonlijke punten</div>
+                    <FeaturedCardStat>32</FeaturedCardStat>
+                  </FeaturedCardTop>
+                  <ProgressBar> </ProgressBar>
+                  <v-card-text>
+                    Behaalde punten
+                    <Badge />
+                  </v-card-text>
+                </FeaturedCard>
+              </v-col>
+
+              <v-col>
+                <FeaturedCard>
+                  <FeaturedCardTop>
+                    <div>Persoonlijke punten</div>
+                    <FeaturedCardStat>32</FeaturedCardStat>
+                  </FeaturedCardTop>
+                  <ProgressBar> </ProgressBar>
+                  <v-card-text>
+                    Behaalde punten
+                    <Badge />
+                  </v-card-text>
+                </FeaturedCard>
+              </v-col>
+            </v-row>
+
+            <v-row>
+              <v-col>
+                <FeaturedCard>
+                  <FeaturedCardTop>
+                    <div>Persoonlijke punten</div>
+                    <FeaturedCardStat>32</FeaturedCardStat>
+                  </FeaturedCardTop>
+                  <ProgressBar> </ProgressBar>
+                  <v-card-text>
+                    Behaalde punten
+                    <Badge />
+                  </v-card-text>
+                </FeaturedCard>
+              </v-col>
+
+              <v-col>
+                <FeaturedCard>
+                  <FeaturedCardTop>
+                    <div>Persoonlijke punten</div>
+                    <FeaturedCardStat>32</FeaturedCardStat>
+                  </FeaturedCardTop>
+                  <ProgressBar> </ProgressBar>
+                  <v-card-text>
+                    Behaalde punten
+                    <Badge />
+                  </v-card-text>
+                </FeaturedCard>
+              </v-col>
+
+              <v-col>
+                <FeaturedCard>
+                  <FeaturedCardTop>
+                    <div>Persoonlijke punten</div>
+                    <FeaturedCardStat>32</FeaturedCardStat>
+                  </FeaturedCardTop>
+                  <ProgressBar> </ProgressBar>
+                  <v-card-text>
+                    Behaalde punten
+                    <Badge />
+                  </v-card-text>
+                </FeaturedCard>
+              </v-col>
+            </v-row>
+
+            <v-row>
+              <v-col>
+                <FeaturedCard>
+                  <FeaturedCardTop>
+                    <div>Persoonlijke punten</div>
+                    <FeaturedCardStat>32</FeaturedCardStat>
+                  </FeaturedCardTop>
+                  <ProgressBar> </ProgressBar>
+                  <v-card-text>
+                    Behaalde punten
+                    <Badge />
+                  </v-card-text>
+                </FeaturedCard>
+              </v-col>
+
+              <v-col> </v-col>
+
+              <v-col> </v-col>
+            </v-row>
           </v-col>
         </v-row>
       </v-container>
     </v-main>
   </v-app>
 </template>
+<script>
+export default {
+  randomColor() {
+    let colors = ["Red", "purple", "Green", "blue", "yellow", "orange"];
+    return colors[Math.floor(math.random() * colors.length)];
+  },
+};
+</script>
---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="ProgressBar">
+    <v-progress-linear
+     buffer-value="60"
+      value="40"
+      stream
+      :color=randomColor()
+      height="10"
+    ></v-progress-linear>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+<script>
+export default {
+    data: () => ({
+randomColor() {
+  let colors= ["red", "purple","green", "blue", "yellow", "orange"];
+  return colors[Math.floor(Math.random() * colors.length)];
+},
+    })}
+</script>
